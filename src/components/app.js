0e92f509520d3ac52d0dfb09eaef314f61bd3a46
Weijia Tang <weijia.tang.18@dartmouth.edu>
2016-07-20 03:09:17 -0400

    basic functionalities

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import Immutable from 'immutable';
+import Note from './note';
 
-import Welcome from './welcome';
+import InputBar from './input_bar';
 
 // example class based component (smart component)
 class App extends Component {
@@ -8,13 +10,78 @@ class App extends Component {
     super(props);
 
     // init component state here
-    this.state = {};
+    this.state = {
+      notes: Immutable.Map(),
+      id: 0,
+    };
   }
+
+  deleteNote(id) {
+    this.setState({
+      notes: this.state.notes.delete(id),
+    });
+  }
+
+  addNote(titleInput) {
+    const note = {
+      title: titleInput,
+      text: 'content',
+      x: 400,
+      y: 12,
+      zIndex: 0,
+      isEditing: false,
+    };
+    this.setState({
+      id: this.state.id + 1,
+      notes: this.state.notes.set(this.state.id, note),
+    });
+  }
+
+  updatePosition(id, x, y) {
+    this.setState({
+      notes: this.state.notes.update(id, (n) => {
+        return Object.assign({}, n, { x }, { y });
+      }),
+    });
+  }
+
+  editNote(id, status) {
+    this.setState({
+      notes: this.state.notes.update(id, (n) => {
+        return Object.assign({}, n, { isEditing: status });
+      }),
+    });
+  }
+
+  editText(id, text) {
+    this.setState({
+      notes: this.state.notes.update(id, (n) => {
+        return Object.assign({}, n, { text });
+      }),
+    });
+  }
+
 
   render() {
     return (
       <div>
-        <Welcome />
+        <InputBar addNote={titleInput => this.addNote(titleInput)} />
+        <div>
+        {this.state.notes.entrySeq().map(([id, note]) => {
+          return (
+            <Note key={id} note={note} id={id} updatePosition={
+              (x, y) => this.updatePosition(id, x, y)
+            } deleteNote={
+              () => this.deleteNote(id)
+            } editNote={
+              (status) => this.editNote(id, status)
+            } editText={
+              (text) => this.editText(id, text)
+            }
+            />
+          );
+        })}
+        </div>
       </div>
     );
   }
---
+++ src/components/note.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import Draggable from 'react-draggable';
+import Textarea from 'react-textarea-autosize';
+import marked from 'marked';
+
+
+// function based "dumb" component with no state
+const Note = (props) => {
+  const onDrag = (e, ui) => {
+    props.updatePosition(ui.x, ui.y);
+  };
+
+  const onStopDrag = (e, ui) => {
+    props.updatePosition(ui.x, ui.y);
+  };
+
+  const onDeleteClick = () => {
+    props.deleteNote();
+  };
+
+  const onEditClick = () => {
+    props.editNote(true);
+  };
+
+  const onEditFinish = () => {
+    props.editNote(false);
+  };
+
+  const onInputChange = (event) => {
+    props.editText(event.target.value);
+  };
+
+  const editing = () => {
+    if (props.note.isEditing) {
+      return (
+        <div className="note">
+          <div className="topBar">
+            <div className="leftBar">
+              <div>{props.note.title}</div>
+              <i onClick={onDeleteClick} className="fa fa-trash-o" />
+              <i onClick={onEditFinish} className="fa fa-check" />
+            </div>
+            <i className="note-mover fa fa-arrows-alt" />
+          </div>
+          <Textarea className="content" onChange={onInputChange} />
+        </div>
+      );
+    } else {
+      return (
+        <div className="note">
+          <div className="topBar">
+            <div className="leftBar">
+              <div>{props.note.title}</div>
+              <i onClick={onDeleteClick} className="fa fa-trash-o" />
+              <i onClick={onEditClick} className="fa fa-pencil" />
+            </div>
+            <i className="note-mover fa fa-arrows-alt" />
+          </div>
+          <div className="content" dangerouslySetInnerHTML={{ __html: marked(props.note.text || '') }} />
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <Draggable
+        handle=".note-mover"
+        grid={[25, 25]}
+        position={{ x: props.note.x, y: props.note.y }}
+        onDrag={onDrag}
+        onStop={onStopDrag}
+      >
+        {editing()}
+      </Draggable>
+    </div>
+  );
+};
+
+
+export default Note;
